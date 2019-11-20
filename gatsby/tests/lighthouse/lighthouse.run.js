c2bb31966e22db531324392972206ee72ae54cb3
Alex Fornuto <alex.fornuto@pantheon.io>
2019-11-19 23:19:21 -0600

    don't throttle me bro

--- gatsby/tests/lighthouse/lighthouse.run.js
+++ gatsby/tests/lighthouse/lighthouse.run.js
@@ -26,7 +26,19 @@ const launchChromeAndRunLighthouse = (
       settings: {
         output: ['html'],
         emulatedFormFactor: 'desktop',
-        useThrottling: false
+        throttling: {
+          // Using a "broadband" connection type
+          // Corresponds to "Dense 4G 25th percentile" in https://docs.google.com/document/d/1Ft1Bnq9-t4jK5egLSOc28IL4TvR-Tt0se_1faTA4KTY/edit#heading=h.bb7nfy2x9e5v
+          rttMs: 40,
+          throughputKbps: 10 * 1024,
+          cpuSlowdownMultiplier: 1,
+        },
+        passes: [
+          {
+            passName: 'onlyPass',
+            useThrottling: false
+          }
+        ]
         // onlyCategories: ['performance']
       },
     }
