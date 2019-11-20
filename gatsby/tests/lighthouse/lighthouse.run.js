3ef3fa691825cba79010f96de182e97055d9afa3
Alex Fornuto <alex.fornuto@pantheon.io>
2019-11-20 01:05:18 -0600

    code cleanup

--- gatsby/tests/lighthouse/lighthouse.run.js
+++ gatsby/tests/lighthouse/lighthouse.run.js
@@ -26,6 +26,7 @@ const launchChromeAndRunLighthouse = (
       settings: {
         output: ['html'],
         emulatedFormFactor: 'desktop',
+        skipAudits: ['is-crawlable'],
         throttling: {
           // Using a "broadband" connection type
           // Corresponds to "Dense 4G 25th percentile" in https://docs.google.com/document/d/1Ft1Bnq9-t4jK5egLSOc28IL4TvR-Tt0se_1faTA4KTY/edit#heading=h.bb7nfy2x9e5v
@@ -33,12 +34,6 @@ const launchChromeAndRunLighthouse = (
           throughputKbps: 10 * 1024,
           cpuSlowdownMultiplier: 1,
         },
-        passes: [
-          {
-            passName: 'onlyPass',
-            skipAudits: ['is-crawlable']
-          },
-        ],
         onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo']
       },
     }
